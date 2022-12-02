00af14223d55ea123d72debb3ddf24816a4ad1b1
lipicoder <lipicoder@qq.com>
2022-12-02 15:02:40 +0800

    add random int to init file

--- src/magicbag/__init__.py
+++ src/magicbag/__init__.py
@@ -1,15 +1,23 @@
 """Magicbag init file."""
 
 from .check import prefix_check
-from .mock.date import random_date, random_datetime
-from .mock.decimal import random_decimal
-from .mock.time import random_timestamp
+from .mock import (
+    random_date,
+    random_datetime,
+    random_decimal,
+    random_int,
+    random_timestamp,
+)
 
-__all__ = ["prefix_check",
-           "random_date",
-           "random_datetime",
-           "random_timestamp",
-           "random_decimal"]
+__all__ = [
+    "prefix_check",
+    "random_date",
+    "random_datetime",
+    "random_timestamp",
+    "random_decimal",
+    "random_int",
+]
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
